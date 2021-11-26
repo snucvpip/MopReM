231ac29c162e09480bb400665f91da466cb5e800
pinggooo <adizer0@snu.ac.kr>
2021-11-26 22:46:17 +0900

    Update main.py

--- impl/pre-processing/main.py
+++ impl/pre-processing/main.py
@@ -1,32 +1,42 @@
+import numpy as np
+
 from pre_processing import *
 import os
 import math
 
 dirName = "data/sample"
 imFilename = os.path.join(dirName, "source.jpg")
-# image = cv2.imread(imFilename)
+image = cv2.imread(imFilename)
 
 
-def Snapshot(image, lim_scale, level=10, stride=None):
+def Snapshot(level=10, nx=10, ny=10):
     #################### EXPLANATION #####################
-    # This function makes snapshots by dividing whole input image according to scale.
-    # We can get lim_scale by GetLimScale() function.
+    # This function makes snapshots of each level
+    # The level is equivalent of magnification (the higher level is, the more magnified image is)
+    # The nx, ny determines dividing range
+    # the cropped images in neighborhood have same area for post-processing
     ######################################################
 
     ##################### PARAMETERS #####################
-    # image: phone-captured image
-    # lim_scale: Maximal magnification of image in which we can recognize the moire pattern
-    # level: log scale level in range of 1 ~ lim_scale
-    # stride: (stride_x, stride_y)
+    # level: division in range (1 ~ nx), (1 ~ ny)
+    # nx: maximum number of dividing in x-axis
+    # ny: maximum number of dividing in y-axis
     ######################################################
 
     ################### USAGE EXAMPLE ####################
-    # If you put in 40 as lim_scale and 10 as level,
-    # this function will make 10 snapshot sets according to incremental log scale as below,
-    # [ 1.  1.50663019  2.26993453  3.41995189  5.15260277  7.76306689, 11.69607095 17.62165361 26.54931532 40.]
+    # Snapshot(5, 2, 2)
+    # This usage will divide image in 5 levels.
+    # The maximum number of dividing is 2 in x, y axis
+    # nx = [1, 3, 5]
+    # ny = [1, 3, 5]
+    # level0 = original input image
+    # level1 = 3 pieces in x-axis X 3 pieces in y-axis = 9 pieces
+    # level2 = 5 pieces in x-axis X 5 pieces in y-axis = 25 pieces
     ######################################################
 
-    scale = np.exp(np.linspace(math.log(1), math.log(lim_scale), level))
+    snapshot_info = []
+    list_nx = np.linspace(1, nx, level)
+    list_ny = np.linspace(1, ny, level)
 
     for i in range(level):
         dir_path = dirName + '/snapshot_level' + str(i)
@@ -34,27 +44,40 @@ def Snapshot(image, lim_scale, level=10, stride=None):
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
 
-        width = int(math.ceil(image.shape[1] / scale[i]))
-        height = int(math.ceil(image.shape[0] / scale[i]))
-
-        if stride is None:
-            stride_i = (width, height)
+        width = int(math.ceil(image.shape[1] * 2 / (list_nx[i] + 1)))
+        height = int(math.ceil(image.shape[0] * 2 / (list_ny[i] + 1)))
 
         num_image = 1
+        stride = (math.floor(width / 2), math.floor(height / 2))
         y_start, y_end = (0, height)
 
-        while y_end <= image.shape[0]:
+        for y in range(int(math.ceil(list_ny[i]))):
             x_start, x_end = (0, width)
 
-            while x_end <= image.shape[1]:
+            for x in range(int(math.ceil(list_nx[i]))):
                 cv2.imwrite(dir_path + '/' + str(num_image) + '.jpg', image[y_start:y_end, x_start:x_end])
-                x_start += stride_i[0]
-                x_end += stride_i[0]
+
+                if list_nx[i] >= 2 and x == list_nx[i] - 2:
+                    x_start = image.shape[1] - 1 - width
+                    x_end = image.shape[1] - 1
+                else:
+                    x_start += stride[0]
+                    x_end += stride[0]
+
                 num_image += 1
 
-            y_start += stride_i[1]
-            y_end += stride_i[1]
+            if list_ny[i] >= 2 and y == list_ny[i] - 2:
+                y_start = image.shape[0] - 1 - height
+                y_end = image.shape[0] - 1
+            else:
+                y_start += stride[1]
+                y_end += stride[1]
+
+        snapshot_info.append(((list_nx[i], list_ny[i]), (width, height)))
+        print('level{} Complete, nx: {}, ny: {}, width: {}, height: {}'.format(i, list_nx[i], list_ny[i], width, height))
+
+    return snapshot_info
 
 
-# if __name__ == "__main__":
-#     Snapshot(40)
+if __name__ == "__main__":
+    Snapshot()
