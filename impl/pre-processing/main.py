dd4ab71acafe7d39e7e355ab4f4aa55ebaf7105d
pinggooo <adizer0@snu.ac.kr>
2021-11-26 19:14:46 +0900

    Update main.py for image augmentation

--- impl/pre-processing/main.py
+++ impl/pre-processing/main.py
@@ -4,11 +4,27 @@ import math
 
 dirName = "data/sample"
 imFilename = os.path.join(dirName, "source.jpg")
-image = cv2.imread(imFilename)
+# image = cv2.imread(imFilename)
 
 
-def Snapshot(lim_scale, level=10, stride=None):
-    # stride = (stride_x, stride_y)
+def Snapshot(image, lim_scale, level=10, stride=None):
+    #################### EXPLANATION #####################
+    # This function makes snapshots by dividing whole input image according to scale.
+    # We can get lim_scale by GetLimScale() function.
+    ######################################################
+
+    ##################### PARAMETERS #####################
+    # image: phone-captured image
+    # lim_scale: Maximal magnification of image in which we can recognize the moire pattern
+    # level: log scale level in range of 1 ~ lim_scale
+    # stride: (stride_x, stride_y)
+    ######################################################
+
+    ################### USAGE EXAMPLE ####################
+    # If you put in 40 as lim_scale and 10 as level,
+    # this function will make 10 snapshot sets according to incremental log scale as below,
+    # [ 1.  1.50663019  2.26993453  3.41995189  5.15260277  7.76306689, 11.69607095 17.62165361 26.54931532 40.]
+    ######################################################
 
     scale = np.exp(np.linspace(math.log(1), math.log(lim_scale), level))
 
@@ -22,23 +38,23 @@ def Snapshot(lim_scale, level=10, stride=None):
         height = int(math.ceil(image.shape[0] / scale[i]))
 
         if stride is None:
-            stride = (width, height)
-
-        x_start, x_end = (0, width)
-        y_start, y_end = (0, height)
+            stride_i = (width, height)
 
         num_image = 1
+        y_start, y_end = (0, height)
 
         while y_end <= image.shape[0]:
+            x_start, x_end = (0, width)
+
             while x_end <= image.shape[1]:
                 cv2.imwrite(dir_path + '/' + str(num_image) + '.jpg', image[y_start:y_end, x_start:x_end])
-                x_start += stride[0]
-                x_end += stride[0]
+                x_start += stride_i[0]
+                x_end += stride_i[0]
                 num_image += 1
 
-            y_start += stride[1]
-            y_end += stride[1]
+            y_start += stride_i[1]
+            y_end += stride_i[1]
 
 
-if __name__ == "__main__":
-    Snapshot(40)
+# if __name__ == "__main__":
+#     Snapshot(40)
