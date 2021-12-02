cf9d83787b4cfefd039e93b9f7f97c3bf15baecc
GilHo-Roh <rgh5102@naver.com>
2021-12-03 00:32:44 +0900

    merge

--- impl/main.py
+++ impl/main.py
@@ -46,7 +46,6 @@ class MopReM:
         for i in range(level):
             nx, ny = info[i][0]
             n = nx*ny
-            print(n)
             for j in range(n):
                 name = str(i)+'-'+str(j+1)+'.png'
                 source = os.path.join(datadir, name)
@@ -57,7 +56,7 @@ class MopReM:
 
 
     def post_process(self):
-        datadir = os.path.join(pardir, 'result/pre_process', self.n)
+        datadir = os.path.join(pardir, 'result/demoire', self.n)
         assert os.path.exists(datadir), print('Post process: datadir not exists')
         resultdir = os.path.join(pardir, 'result/post_process', self.n)
         if not os.path.exists(resultdir):
@@ -71,26 +70,43 @@ class MopReM:
         info = pre_process.pre_process.snapshot_info
         level = len(info)
         img_width, img_height = info[0][1]
-
+        filename = '0-1.png'
+        filepath = os.path.join(datadir, filename)
+        img_zero = cv2.imread(filepath,  cv2.IMREAD_COLOR)
+        result_name = 'clean0.png'
+        result_path = os.path.join(resultdir, result_name)
+        cv2.imwrite(result_path, img_zero)
         for i in range(1, level):
-            img_merge = np.empty(shape=(img_width, img_height, 3))
+            img_merge = np.empty(shape=(img_height, img_width, 3))
+            acc_array = np.empty(shape=(img_height, img_width, 3))
             nx, ny = info[i][0]
             partial_width, partial_height = info[i][1]
             stride = info[i][2]
             num = 1
-
+            
             for y in range(ny):
                 if y != ny - 1:
                     for x in range(nx):
                         filename = str(i)+'-'+str(num)+'.png'
                         filepath = os.path.join(datadir, filename)
                         partial_image = cv2.imread(filepath,  cv2.IMREAD_COLOR)
+                        temp = np.empty(shape=(partial_width, partial_height))
                         if x == nx - 1:
+                            temp = img_merge[y * stride[1]: y * stride[1] + partial_height,\
+                                            img_width-1-partial_width:img_width-1]
+                            temp = (temp+partial_image)                         
                             img_merge[y * stride[1]: y * stride[1] + partial_height,\
-                                        img_width-1-partial_width:img_width-1] = partial_image
+                                    img_width-1-partial_width:img_width-1] = temp
+                            acc_array[y * stride[1]: y * stride[1] + partial_height,\
+                                    img_width-1-partial_width:img_width-1] += 1
                         else:
+                            temp = img_merge[y * stride[1]: y * stride[1] + partial_height,\
+                                            x * stride[0]: x * stride[0] + partial_width]
+                            temp = (temp+partial_image)
                             img_merge[y * stride[1]: y * stride[1] + partial_height,\
-                                        x * stride[0]: x * stride[0] + partial_width] = partial_image
+                                    x * stride[0]: x * stride[0] + partial_width] = temp
+                            acc_array[y * stride[1]: y * stride[1] + partial_height,\
+                                    x * stride[0]: x * stride[0] + partial_width] += 1
                         num += 1
                 else:
                     for x in range(nx):
@@ -98,16 +114,26 @@ class MopReM:
                         filepath = os.path.join(datadir, filename)
                         partial_image = cv2.imread(filepath,  cv2.IMREAD_COLOR)
                         if x == nx - 1:
+                            temp = img_merge[img_height-1-partial_height:img_height-1,\
+                            img_width-1-partial_width:img_width-1]
+                            temp = (temp+partial_image)
                             img_merge[img_height-1-partial_height:img_height-1,\
-                            img_width-1-partial_width:img_width-1] = partial_image
+                                    img_width-1-partial_width:img_width-1] = temp
+                            acc_array[img_height-1-partial_height:img_height-1,\
+                                    img_width-1-partial_width:img_width-1] += 1
                         else:
+                            temp = img_merge[img_height-1-partial_height:img_height-1,\
+                            x * stride[0]: x * stride[0] + partial_width]
+                            temp = (temp+partial_image)
                             img_merge[img_height-1-partial_height:img_height-1,\
-                            x * stride[0]: x * stride[0] + partial_width] = partial_image
+                                    x * stride[0]: x * stride[0] + partial_width] = temp
+                            acc_array[img_height-1-partial_height:img_height-1,\
+                                    x * stride[0]: x * stride[0] + partial_width] += 1
                         num += 1
 
             result_name = 'clean'+str(i)+'.png'
             result_path = os.path.join(resultdir, result_name)
-            cv2.imwrite(result_path, img_merge)
+            cv2.imwrite(result_path, img_merge/acc_array)
 
         # post_process
         im = cv2.imread(source, cv2.IMREAD_COLOR)
